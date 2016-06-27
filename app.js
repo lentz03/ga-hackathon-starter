4da44fb07a383d14200eaba9baa93265d370eaeb
Lentz <lentz03@live.com>
2016-06-27 16:26:02 +0800

    first time

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ const homeController = require('./controllers/home');
 const userController = require('./controllers/user');
 const apiController = require('./controllers/api');
 const contactController = require('./controllers/contact');
+const guidesController = require ('./controllers/guides')
 
 /**
  * API keys and Passport configuration.
@@ -104,6 +105,7 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }))
  * Primary app routes.
  */
 app.get('/', homeController.index);
+app.get('/guides', guidesController.index);
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.get('/logout', userController.logout);
---
+++ controllers/guides.js
@@ -0,0 +1,5 @@
+exports.index = (req, res) => {
+  res.render('guides', {
+    title: 'Guides'
+  });
+};
